c055cd73fe10f9d2d9d54e2141595c3030004f21
Stefano Casasola <ste.casasola@gmail.com>
2021-05-27 12:16:12 +0200

    Finishes the lecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
+import { Component } from 'react'
 import './App.css'
 import Header from './components/Header'
 import DetailComponent from './components/DetailComponent'
 
 import data from './data/data.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header title="Title passed from App" subTitle="show this only if present" />
-        <Header title="another one" />
-        <DetailComponent data={data} />
-      </header>
-    </div>
-  )
+import { IData } from './types/types'
+import { Route } from 'react-router-dom'
+
+interface AppState {
+  data: IData[]
+}
+
+class App extends Component<{}, AppState> {
+  state: AppState = {
+    data: [],
+  }
+
+  componentDidMount = () => this.setState({ data: data })
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header title="Title passed from App" subTitle="show this only if present" />
+          <Header title="another one" />
+          <Route
+            path="/detail"
+            exact
+            render={(routerProps) => <DetailComponent {...routerProps} data={this.state.data} />}
+          />
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
--- src/components/DetailComponent.tsx
+++ src/components/DetailComponent.tsx
@@ -1,8 +1,41 @@
 import { Component } from 'react'
+import { IData } from '../types/types'
+import { RouteComponentProps } from 'react-router-dom'
 
-class DetailComponent extends Component {
+interface DetailComponentProps {
+  data: IData[]
+}
+
+// with the Route our props are enhanced
+// we're getting data, but we're also getting history, location and match
+
+// & is TYPE INTERSECTION, which means ALL THE DEFINITIONS, it' going to MERGE all of them in a single definition
+
+// | -> UNION is for picking a type between several of those
+
+// ------
+type MyPropsMerged = DetailComponentProps & RouteComponentProps
+
+interface DetailComponentPropsAndMore extends RouteComponentProps {
+  data: IData[]
+}
+// ------
+
+class DetailComponent extends Component<DetailComponentPropsAndMore, {}> {
+  componentDidMount = () => {
+    console.log(this.props.history)
+  }
+
   render() {
-    return <h1>DETAIL COMPONENT</h1>
+    return (
+      <div>
+        {this.props.data
+          .filter((obj) => obj.isValid)
+          .map((obj) => (
+            <div key={obj.id}>{obj.description}</div>
+          ))}
+      </div>
+    )
   }
 }
 
